c5073652c7e874e113661fb43c22024cbcfb2ec1
Alexandr <tel1tsyn@yandex.ru>
2023-09-15 21:46:36 +0300

    fixed errors

--- tree.c
+++ tree.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "tree.h"
 
 euler_tree *euler_plant_tree(euler_tree *target) {
     target->density = 0;
     target->krone = NULL;
+    return target;
 }
 
 void euler_fell_tree(euler_tree *target) {
@@ -18,9 +20,13 @@ void euler_add_node_tree(euler_tree *target, void *content, unsigned int size) {
         target->krone = malloc(size);
         /* error malloc need treat */
     } else {
-        target->krone = realloc(target->density * size + size);
+        target->krone = realloc(target->krone, target->density * size + size);
         /* error realloc need treat */
     }
     target->krone[target->density] = content;
     target->density += 1;
 } 
+
+int main() {
+    return 0;
+}
